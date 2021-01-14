75bd702c094c481ee9766c468b4369dfdb20aba7
Jane <heilea@hotmail.fr>
2021-01-14 22:44:14 +0100

    Add Timer

--- New Unity Project/Assets/Scripts/Game.cs
+++ New Unity Project/Assets/Scripts/Game.cs
@@ -22,7 +22,12 @@ public class Game : MonoBehaviour
     public GameObject   DogPrefab;
     public GameObject   PlayerPrefab;
 
-    private Text TimeText;
+    public  Text TimeText;
+    public  Text TimerText;
+
+    public float TimeAGame = 60.0f;
+
+    private float TimeStartGame = 0.0f;
     public GameState gameState {  get { return _gameState;  } }
     GameState _gameState;
 
@@ -36,14 +41,35 @@ public class Game : MonoBehaviour
         _gameState = GameState.GS_MENU;
 
         TimePanel.SetActive(false);
-        TimeText = TimePanel.GetComponentInChildren<Text>();
 
         StartGame();
     }
 
+    string GetTime()
+    {
+        float timer = TimeAGame - Time.time - TimeStartGame;
+        float minutes = Mathf.Floor(timer / 60);
+        float seconds = Mathf.RoundToInt(timer % 60);
+
+        string 
+            s_minutes = minutes.ToString(),
+            s_seconds = seconds.ToString();
+
+        if (minutes < 10)
+        {
+            s_minutes = "0" + minutes.ToString();
+        }
+        if (seconds < 10)
+        {
+            s_seconds = "0" + Mathf.RoundToInt(seconds).ToString();
+        }
+        return "Time   " + s_minutes + ':' + s_seconds;
+    }
     // Update is called once per frame
     void Update()
     {
+        TimerText.text = GetTime();
+
         switch (_gameState)
         {
             case GameState.GS_MENU:
@@ -74,8 +100,9 @@ public class Game : MonoBehaviour
             case GameState.GS_GAME:
                 {
                     MainTitlePanel.SetActive(false);
-                    TimePanel.SetActive(false);
-                    Time.timeScale = 1;
+                    TimeText.enabled = false;
+                    TimerText.enabled = true;
+                    TimeStartGame = Time.time;
                 }
                 break;
 
@@ -89,7 +116,8 @@ public class Game : MonoBehaviour
                     }
                     StartCoroutine("Timer");
                     TimePanel.SetActive(true);
-
+                    TimerText.enabled = false;
+                    TimeText.enabled = true;
                     //Time.timeScale = 0;
                 }
                 break;
@@ -101,18 +129,18 @@ public class Game : MonoBehaviour
     IEnumerator Timer()
     {
         TimeText.text = "Ready ? ";
-             yield return new WaitForSeconds(1.5f);
-        TimeText.text = "Let's Gooooo ! ";
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(0.75f);
         TimeText.text = "3";
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
 
         TimeText.text = "2";
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
 
         TimeText.text = "1";
-        yield return new WaitForSeconds(1f);
-
+        yield return new WaitForSeconds(0.5f);
+  
+        TimeText.text = "Let's Gooooo ! ";
+        yield return new WaitForSeconds(0.5f);
         SetState(GameState.GS_GAME);
     }
 
